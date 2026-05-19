473bcaa4e003a8e5f04ae9b496446eacc1ea4904
meowpow-png <marin.softic@proton.me>
2026-05-19 20:46:53 +0200

    Annotate production packages as NullMarked

---
+++ src/main/java/io/github/meowpowpng/gatlingfx/http/package-info.java
@@ -0,0 +1,4 @@
+@NullMarked
+package io.github.meowpowpng.gatlingfx.http;
+
+import org.jspecify.annotations.NullMarked;
---
+++ src/main/java/io/github/meowpowpng/gatlingfx/proxy/package-info.java
@@ -0,0 +1,4 @@
+@NullMarked
+package io.github.meowpowpng.gatlingfx.proxy;
+
+import org.jspecify.annotations.NullMarked;
