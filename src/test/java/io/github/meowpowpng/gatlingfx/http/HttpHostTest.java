459c2cb7e28a6e339410e5569a95b31363f3bc5b
meowpow-png <marin.softic@proton.me>
2026-05-20 16:58:39 +0200

    Implement http host as value object

---
+++ src/test/java/io/github/meowpowpng/gatlingfx/http/HttpHostTest.java
@@ -0,0 +1,33 @@
+package io.github.meowpowpng.gatlingfx.http;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
+
+class HttpHostTest {
+
+    @Test
+    @SuppressWarnings("DataFlowIssue")
+    @DisplayName("Should throw NullPointerException when value is null")
+    void should_ThrowNullPointerException_when_ValueIsNull() {
+        assertThatThrownBy(() -> new HttpHost(null))
+                .isInstanceOf(NullPointerException.class);
+    }
+
+    @Test
+    @DisplayName("Should throw IllegalArgumentException when value is blank")
+    void should_ThrowIllegalArgumentException_when_ValueIsBlank() {
+        assertThatThrownBy(() -> new HttpHost(" "))
+                .isInstanceOf(IllegalArgumentException.class);
+    }
+
+    @Test
+    @DisplayName("Should create host from provided value")
+    void should_CreateHost_when_ValidValueIsProvided() {
+        var host = "example.com";
+
+        assertThat(HttpHost.of(host)).isEqualTo(new HttpHost(host));
+    }
+}
