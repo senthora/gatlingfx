4a080ec042337e82f6518851e085cce202e6199c
meowpow-png <marin.softic@proton.me>
2026-05-23 17:45:58 +0200

    Add note about simulation short-circuiting

--- runtime/src/main/java/com/senthora/gatlingfx/runtime/core/api/SimulationRuntime.java
+++ runtime/src/main/java/com/senthora/gatlingfx/runtime/core/api/SimulationRuntime.java
@@ -10,6 +10,11 @@ public interface SimulationRuntime {
 
     /**
      * Executes the provided Gatling simulations.
+     * <p>
+     * <strong>API Note:</strong>
+     * Simulation execution does not short-circuit when
+     * individual simulations fail. All provided simulations
+     * are executed before returning execution results.
      *
      * @param simulationClasses simulation classes to execute
      * @return execution results
