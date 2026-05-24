9f7c3df6692c30611fc6bf8ea1d4bff34d03a230
meowpow-png <marin.softic@proton.me>
2026-05-24 15:55:16 +0200

    Annotate proxy gatling simulations

--- proxy/src/integrationTest/java/com/senthora/gatlingfx/proxy/simulation/CoffeeHttpsRoutingSimulation.java
+++ proxy/src/integrationTest/java/com/senthora/gatlingfx/proxy/simulation/CoffeeHttpsRoutingSimulation.java
@@ -2,12 +2,14 @@ package com.senthora.gatlingfx.proxy.simulation;
 
 import com.senthora.gatlingfx.proxy.ProxyProtocols;
 import com.senthora.gatlingfx.proxy.support.ProxyFixtures;
+import com.senthora.gatlingfx.runtime.core.api.GatlingSimulation;
 import com.senthora.gatlingfx.simulation.api.BaseSimulation;
 import com.senthora.gatlingfx.simulation.api.SimulationProtocol;
 import com.senthora.gatlingfx.simulation.api.SimulationScenario;
 
 import java.util.List;
 
+@GatlingSimulation
 public final class CoffeeHttpsRoutingSimulation extends BaseSimulation {
 
     @Override
