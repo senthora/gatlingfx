98a6ba522b50a66b48c16611d27fd0a6ad438326
meowpow-png <marin.softic@proton.me>
2026-05-25 13:42:57 +0200

    Resolve utility class fake polymorphism

--- runtime/src/main/java/com/senthora/gatlingfx/runtime/core/internal/ClasspathSimulationScanner.java
+++ runtime/src/main/java/com/senthora/gatlingfx/runtime/core/internal/ClasspathSimulationScanner.java
@@ -11,18 +11,16 @@ import java.util.List;
  * Internal runtime classpath scanner
  * used by {@link SimulationScanner}.
  */
-public final class ClasspathSimulationScanner implements SimulationScanner {
+public final class ClasspathSimulationScanner {
 
-    private static final ClassGraph CLASS_GRAPH = classGraph();
     private static final String ANNOTATION_NAME = GatlingSimulation.class.getName();
+    private static final ClassGraph CLASS_GRAPH = new ClassGraph()
+            .enableClassInfo()
+            .enableAnnotationInfo();
 
     public static List<Class<?>> scan() {
         try (var scan = CLASS_GRAPH.scan()) {
             return scan.getClassesWithAnnotation(ANNOTATION_NAME).loadClasses();
         }
-    }
-
-    private static ClassGraph classGraph() {
-        return new ClassGraph().enableClassInfo().enableAnnotationInfo();
     }
 }
