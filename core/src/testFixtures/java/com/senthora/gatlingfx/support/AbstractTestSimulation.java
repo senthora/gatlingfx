e20dc03b7668d1b37d966ac32a66bb69361db8c2
meowpow-png <marin.softic@proton.me>
2026-05-28 16:27:42 +0200

    Create abstract test simulation class

---
+++ core/src/testFixtures/java/com/senthora/gatlingfx/support/AbstractTestSimulation.java
@@ -0,0 +1,13 @@
+package com.senthora.gatlingfx.support;
+
+import com.senthora.gatlingfx.http.api.HttpBaseUrl;
+import com.senthora.gatlingfx.simulation.api.BaseSimulation;
+import com.senthora.gatlingfx.simulation.api.SimulationProtocol;
+
+public abstract class AbstractTestSimulation extends BaseSimulation {
+
+    @Override
+    protected SimulationProtocol protocol() {
+        return SimulationProtocol.create().baseUrl(HttpBaseUrl.LOCALHOST);
+    }
+}
--- core/src/testFixtures/java/com/senthora/gatlingfx/support/TestSimulation.java
+++ core/src/testFixtures/java/com/senthora/gatlingfx/support/TestSimulation.java
@@ -1,13 +1,3 @@
 package com.senthora.gatlingfx.support;
 
-import com.senthora.gatlingfx.http.api.HttpBaseUrl;
-import com.senthora.gatlingfx.simulation.api.BaseSimulation;
-import com.senthora.gatlingfx.simulation.api.SimulationProtocol;
-
-public class TestSimulation extends BaseSimulation {
-
-    @Override
-    protected SimulationProtocol protocol() {
-        return SimulationProtocol.create().baseUrl(HttpBaseUrl.LOCALHOST);
-    }
-}
+public class TestSimulation extends AbstractTestSimulation {}
